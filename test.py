7041092abc145f09d7c8e7944860f2dc7cb957e1
Shreyansh Sharma <shreyansh.ssharma@gmail.com>
2025-08-18 11:06:30 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEST_DIR = os.path.join(BASE_DIR, "test_images")
 
-#This script is only for path and directory check 
+#This script is only for path and directory check.
+#Any other library checks and test commands can also be made it.
+#It is not related to main code.(Detect.py)
